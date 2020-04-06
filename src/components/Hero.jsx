2a22a70f8c53df0789f072b6ff59dca97b377bf5
Aris Razuri <arazuli4@gmail.com>
2020-04-05 23:01:40 -0700

    styled main section with logo and links to github and setup download

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Hero = () => {
   return (
     <section id="hero">
-      <h2>Nautilus</h2>
-      <p>A Docker Compose Charting Tool</p>
+      <div>
+        <h2>Nautilus</h2>
+        <p>A Docker Compose Charting Tool</p>
+        <button>
+          <FontAwesomeIcon icon={faGithub} />
+          <a
+            href="https://github.com/oslabs-beta/nautilus"
+            alt="oslabs-nautilus-github-page"
+            target="Blank"
+          >
+            Github
+          </a>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faChevronCircleDown} />
+          Download
+        </button>
+      </div>
+      <img src="../assets/nautilus_main_logo.svg" alt="nautilus-logo" />
     </section>
   );
 };
--- src/components/Intro.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ScrollAnimation from "react-animate-on-scroll";
-
-const Intro = () => {
-  return <section></section>;
-};
-
-export default Intro;
