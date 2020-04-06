c0d71349ad970289fbfdf054f02cd7ca7adea292
Sweet T <thurtt@rocketmail.com>
2020-04-06 12:59:59 -0700

    Before push up

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,44 +1,44 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faLinux,
   faWindows,
   faApple,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons';
 
 const Download = () => {
   return (
-    <div id="download-title">
+    <div id='download-title'>
       <h2>Download</h2>
-      <div id="download">
-        <div>
+      <div id='download'>
+        <div className='download-icon'>
           <a
-            href="www.google.com"
-            className="download-icons animated flipInX"
-            alt="download-for-linux"
+            href='www.google.com'
+            className='download-icons animated flipInX'
+            alt='download-for-linux'
           >
-            <FontAwesomeIcon icon={faLinux} size="6x" color="#e0e9f1" />
+            <FontAwesomeIcon icon={faLinux} size='6x' color='#e0e9f1' />
             <p>Linux</p>
           </a>
         </div>
-        <div>
+        <div className='download-icon'>
           <a
-            href="www.google.com"
-            className="download-icons animated flipInX"
-            alt="download-for-windows"
+            href='www.google.com'
+            className='download-icons animated flipInX'
+            alt='download-for-windows'
           >
-            <FontAwesomeIcon icon={faWindows} size="6x" color="#e0e9f1" />
+            <FontAwesomeIcon icon={faWindows} size='6x' color='#e0e9f1' />
             <p>Windows</p>
           </a>
         </div>
-        <div>
+        <div className='download-icon'>
           <a
-            href="www.google.com"
-            className="download-icons animated flipInX"
-            alt="download-for-mac"
+            href='www.google.com'
+            className='download-icons animated flipInX'
+            alt='download-for-mac'
           >
-            <FontAwesomeIcon icon={faApple} size="6x" color="#e0e9f1" />
-            <p>Mac</p>
+            <FontAwesomeIcon icon={faApple} size='6x' color='#e0e9f1' />
+            <p>Mac OS</p>
           </a>
         </div>
       </div>
