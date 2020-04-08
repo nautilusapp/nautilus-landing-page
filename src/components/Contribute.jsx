34c314ca7f922fa4820e68578d1c3ce6b4e1d9e1
Aris Razuri <arazuli4@gmail.com>
2020-04-08 13:40:01 -0700

    add slack

--- src/components/Contribute.jsx
+++ src/components/Contribute.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare, faSlack } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Contribute = () => {
   return (
     <section id="contribute">
-      <h1>Contribute to Nautilus</h1>
-      <h3>An open source project on Github</h3>
-      <figure>
-        <ScrollAnimation animateIn="slideInLeft">
-          <figcaption>
-            <a
-              href="https://github.com/oslabs-beta/nautilus"
-              target="Blank"
-              alt="Nautilus GitHub page"
-            >
-              <FontAwesomeIcon
-                icon={faGithubSquare}
-                size="8x"
-                color="#2274a5"
-                className="github-icon"
-              />
-            </a>
-          </figcaption>
-        </ScrollAnimation>
-      </figure>
+      <div>
+        <h2>Contribute to Nautilus</h2>
+        <h3>An open source project on Github</h3>
+        <figure>
+          <ScrollAnimation animateIn="slideInLeft">
+            <figcaption>
+              <a
+                href="https://github.com/oslabs-beta/nautilus"
+                target="Blank"
+                alt="Nautilus GitHub page"
+              >
+                <FontAwesomeIcon
+                  icon={faGithubSquare}
+                  size="8x"
+                  color="#2274a5"
+                  className="github-icon"
+                />
+              </a>
+            </figcaption>
+          </ScrollAnimation>
+        </figure>
+      </div>
+      <div id="line"></div>
+      <div>
+        <h2>Join Us on Slack</h2>
+        <figure>
+          <ScrollAnimation animateIn="slideInRight">
+            <figcaption>
+              <a
+                href="https://join.slack.com/t/nautiluscommunity/shared_invite/zt-deax51kl-PiLLyBiEc0gVFKM_XrwNKQ"
+                target="Blank"
+                alt="Nautilus Slack page"
+              >
+                <FontAwesomeIcon
+                  icon={faSlack}
+                  size="8x"
+                  color="#2274a5"
+                  className="github-icon"
+                />
+              </a>
+            </figcaption>
+          </ScrollAnimation>
+        </figure>
+      </div>
     </section>
   );
 };
